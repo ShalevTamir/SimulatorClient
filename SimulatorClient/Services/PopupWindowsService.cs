cc62f81189c4cd28435600a90f6eb518d190a1cf
Shalev Tamir <shalevtamir1009@gmail.com>
2024-04-04 11:30:04 +0300

    add AddParameterWindow

---
+++ SimulatorClient/Services/PopupWindowsService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace SimulatorClient.Services
+{
+    internal class PopupWindowsService
+    {
+        private static PopupWindowsService _instance;
+        public static PopupWindowsService Instance
+        {
+            get
+            {
+                _instance ??= new PopupWindowsService();
+                return _instance;
+            }
+            private set { }
+        }
+
+        private List<Window> windowInstances;
+
+        private PopupWindowsService()
+        {
+            windowInstances = new List<Window>();
+        }
+
+        public void PopupWindow<WindowType>() where WindowType : Window, new()
+        {
+            var windowInstance = windowInstances.Find(window => window is WindowType);
+            if (windowInstance == null || !windowInstance.IsLoaded)
+            {
+                windowInstances.Remove(windowInstance);
+                windowInstance = new WindowType();
+                windowInstances.Add(windowInstance);
+            }
+
+            if (!windowInstance.IsLoaded)
+            {
+                windowInstance.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+                windowInstance.Show();  
+            }
+            else
+            {
+                windowInstance.Focus();
+            }
+        }
+    }
+}
---
+++ SimulatorClient/Views/AddParameterWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using SimulatorClient.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SimulatorClient.Views
+{
+    /// <summary>
+    /// Interaction logic for AddParameterWindow.xaml
+    /// </summary>
+    public partial class AddParameterWindow : Window
+    {
+        WindowControlsService _windowControlsService;
+        public AddParameterWindow()
+        {
+            _windowControlsService = WindowControlsService.Instance;
+            InitializeComponent();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            _windowControlsService.Minimize(this);
+        }
+
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            _windowControlsService.Maximize(this);
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            _windowControlsService.Close(this);
+        }
+    }
+}
